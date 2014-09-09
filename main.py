1521ddc909c9a52feac10739ef4937e0a3836bcc
Anton Nikulin <dr.tallin@gmail.com>
2014-09-09 23:01:00 +0300

    Loading and Animating the Panda Model

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+"""
+Panda 1.8.1
+"""
 from math import pi, sin, cos
 
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
+from direct.actor.Actor import Actor
 
 class MyApp(ShowBase):
     def __init__(self):
@@ -17,6 +21,13 @@ class MyApp(ShowBase):
 
         # Add the spinCameraTask procedure to the task manager.
         self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+
+        # Load and transform the panda actor
+        self.pandaActor = Actor("models/panda-model", {"walk": "models/panda-walk4"})
+        self.pandaActor.setScale(0.005, 0.005, 0.005)
+        self.pandaActor.reparentTo(self.render)
+        # Loop its animation.
+        self.pandaActor.loop("walk")
 
     # Define a procedure to move the camera.
     def spinCameraTask(self, task):
